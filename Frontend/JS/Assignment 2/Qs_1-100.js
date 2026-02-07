dc23ab791a2afce197e0bff51bf81bf1f9195517
Prince mali <maliprince909@gmail.com>
2026-02-07 23:45:34 +0530

    Frontend Js Assignment 2

---
+++ Frontend/JS/Assignment 2/Qs_1-100.js
@@ -0,0 +1,102 @@
+// Type Coercion & Type Conversion – 100 Logical Questions ---
+
+console.log(1+"2"); // 12
+console.log("1"+2+3); // 123
+console.log(1+2+"3"); // 33
+console.log("10"+5); // 105
+console.log(5+ +"5"); // 10
+console.log("5"-"2"); // 3
+console.log("10"*"2"); // 20
+console.log("5"-true); // 4
+console.log(true+true); // 2
+console.log(null+1); // 1
+console.log(undefined + 1); // NaN
+console.log("5"==5); // true
+console.log("5"===5); // false
+console.log(0==false); // true
+console.log(0===false); // false
+console.log(+"5"); // 5
+console.log(+true); // 1
+console.log(+[]); // 0
+console.log([]+[]); // 
+console.log([]==false); // true
+console.log([0]==false); // true
+console.log({}=={}); // false
+console.log(1 + true + "5"); // 25
+console.log("5"-true+null); // 4
+console.log(!!"false"); // true
+console.log(0.1 + 0.2 == 0.3); // false
+console.log(NaN == NaN); // false
+console.log(""== 0); // true
+console.log(null >= 0); // true
+console.log(null == 0); // false
+console.log(1 + "2"); // 12
+console.log("1" + 2 + 3); // 123
+console.log(1 + 2 + "3"); // 33
+console.log("10" + 5); // 105
+console.log(5+ +"5"); // 10
+console.log("5" - "2"); // 3
+console.log("10"*"2"); // 20
+console.log("5" - true); // 4
+console.log(true + true); // 2
+console.log(null + 1); // 1
+console.log(undefined + 1); // NaN
+console.log("5" == 5); //  true
+console.log("5" === 5); // false
+console.log(0 == false); // true
+console.log(0 === false); // false
+console.log(+"5"); // 5
+console.log(+true); // 1
+console.log(+[]); // 0
+console.log([] + []); // 
+console.log([] == false); // true
+console.log([0] == false); // true
+console.log({} == {}); // false
+console.log(1 + true + "5"); // 25
+console.log("5" - true + null); // 4
+console.log(!!"false"); // true
+console.log(0.1 + 0.2 == 0.3); // false
+console.log(NaN == NaN); // false
+console.log("" == 0); // true
+console.log(null >= 0); // true
+console.log(null == 0); // false
+console.log(1 + "2"); // 12
+console.log("1" + 2 + 3); // 123
+console.log(1 + 2 + "3"); // 33
+console.log("10" + 5); // 105
+console.log(5+ +"5"); // 10
+console.log("5" - "2"); // 3
+console.log("10" * "2"); // 20
+console.log("5" - true); // 4
+console.log(true + true); // 2
+console.log(null + 1); // 1
+console.log(undefined + 1); // NaN
+console.log("5" == 5); // true
+console.log("5" === 5); // false
+console.log(0 == false); // true
+console.log(0 === false); // false
+console.log(+"5"); // 5
+console.log(+true); // 1
+console.log(+[]); // 0
+console.log([] + []); // 
+console.log([] == false); // true
+console.log([0] == false); // true
+console.log({} == {}); // false
+console.log(1 + true + "5"); // 25
+console.log("5" - true + null); // 4
+console.log(!!"false"); // true
+console.log(0.1 + 0.2 == 0.3); // false
+console.log(NaN == NaN); // false
+console.log("" == 0); // true
+console.log(null >= 0); // true
+console.log(null == 0); // false
+console.log(1 + "2"); // 12
+console.log("1" + 2 + 3); // 123
+console.log(1 + 2 + "3"); // 33
+console.log("10" + 5); // 105
+console.log(5+ +"5"); // 10
+console.log("5" - "2"); // 3
+console.log("10"*"2"); // 20
+console.log("5"-true); // 4
+console.log(true + true); // 2
+console.log(null + 1); // 1
